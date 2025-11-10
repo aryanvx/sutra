b633c5bb85ad9e08e9a0d083ad76dab18077b86c
Aryan Vyahalkar <107427986+aryanvx@users.noreply.github.com>
2025-11-10 14:27:35 -0500

    Initial Commit

---
+++ Sutra/SutraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SutraApp.swift
+//  Sutra
+//
+//  Created by Aryan Vyahalkar on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SutraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
